38a5b60befce0b3b102d4d690a2c1bc056ee970d
Amr Hassan <amr.hassan@gmail.com>
2017-11-04 16:19:25 +0100

    Tidying up clap logic

--- src/main.rs
+++ src/main.rs
@@ -22,36 +22,80 @@ use server::ServerInfo;
 
 fn main() {
 
-    const BEARER_TOKEN_ARG_NAME: &str = "bearer_token";
-    const NAKADI_URL_ARG_NAME: &str = "nakadi_url";
-    const METRICS_SUBCOMMAND_NAME: &str = "metrics";
-    const ZIGN_ARG_NAME: &str = "zign";
-    let matches = App::new("CLI Client for Nakadi")
+    struct FlagName;
+    impl FlagName {
+        const ZIGN: &'static str = "zign";
+    }
+
+    struct OptionName;
+    impl OptionName {
+        const BEARER_TOKEN: &'static str = "bearer_token";
+        const NAKADI_URL: &'static str = "nakadi_url";
+    }
+
+    struct SubCommandName;
+    impl SubCommandName {
+        const METRICS: &'static str = "metrics";
+        const EVENT: &'static str = "event";
+        const EVENT_PUBLISH: &'static str = "publish";
+    }
+
+    let option_bearer_token = Arg::with_name(OptionName::BEARER_TOKEN)
+        .long("bearer-token")
+        .value_name("TOKEN")
+        .help("Bearer token value")
+        .env("BEARER_TOKEN")
+        .global(true)
+        .conflicts_with(FlagName::ZIGN);
+
+    let option_nakadi_url = Arg::with_name(OptionName::NAKADI_URL)
+        .long("url")
+        .value_name("NAKADI_URL_BASE")
+        .help("scheme://hostname:[port] of the Nakadi server")
+        .env("NAKADI_URL").global(true);
+
+    let flag_zign = Arg::with_name(FlagName::ZIGN)
+        .long("zign")
+        .help("Use zign to acquire a Bearer token")
+        .takes_value(false)
+        .global(true)
+        .conflicts_with(OptionName::BEARER_TOKEN);
+
+    let subcommand_metrics = SubCommand::with_name(SubCommandName::METRICS)
+        .about("Gets monitoring metrics");
+
+    let subcommand_events_publish = SubCommand::with_name(SubCommandName::EVENT_PUBLISH).about("Publish events");
+
+    let subcommand_events = SubCommand::with_name(SubCommandName::EVENT).about("Events of a certain type").subcommand(subcommand_events_publish);
+
+    let app = App::new("CLI Client for Nakadi")
         .setting(AppSettings::SubcommandRequired)
-        .arg(Arg::with_name(BEARER_TOKEN_ARG_NAME).long("bearer-token").value_name("TOKEN").help("Bearer token value").env("BEARER_TOKEN").global(true).conflicts_with(ZIGN_ARG_NAME))
-        .arg(Arg::with_name(NAKADI_URL_ARG_NAME).long("url").value_name("NAKADI_URL_BASE").help("scheme://hostname:[port] of the Nakadi server").env("NAKADI_URL").global(true))
-        .arg(Arg::with_name(ZIGN_ARG_NAME).long("zign").help("Use zign to acquire a Bearer token").takes_value(false).global(true).conflicts_with(BEARER_TOKEN_ARG_NAME))
-        .subcommand(SubCommand::with_name(METRICS_SUBCOMMAND_NAME).about("Gets monitoring metrics"))
-        .get_matches();
+        .arg(option_bearer_token)
+        .arg(option_nakadi_url)
+        .arg(flag_zign)
+        .subcommand(subcommand_metrics)
+        .subcommand(subcommand_events);
+
+    let matches = app.get_matches();
 
     let authorization =
-        if matches.occurrences_of(ZIGN_ARG_NAME) > 0 {
+        if matches.occurrences_of(FlagName::ZIGN) > 0 {
             server::Authorization::Zign
-        } else if let Some(bearer_token) = matches.value_of(BEARER_TOKEN_ARG_NAME) {
+        } else if let Some(bearer_token) = matches.value_of(OptionName::BEARER_TOKEN) {
             server::Authorization::BearerToken(bearer_token)
         } else {
             server::Authorization::None
         };
 
     let server_info = ServerInfo {
-        url_base: matches.value_of(NAKADI_URL_ARG_NAME).unwrap_or("http://localhost"),
+        url_base: matches.value_of(OptionName::NAKADI_URL).unwrap_or("http://localhost"),
         authorization,
     };
 
     let mut application = Application::new();
 
     match matches.subcommand_name() {
-        Some(METRICS_SUBCOMMAND_NAME) => command_metrics::run(server_info, &mut application),
+        Some(SubCommandName::METRICS) => command_metrics::run(server_info, &mut application),
         _ => panic!("No subcommand is provided, but clap should have handled this already!")
     }
 }
